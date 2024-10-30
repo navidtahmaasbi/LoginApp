f8b464062ca7807ee4f78c8f504c12dce73108b0
[navid] <[navidtahmaasbi@gmail.com]>
2024-10-30 16:57:46 +0330

    2nd

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.loginapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,6 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set the title
+        getSupportActionBar().setTitle("Firebase App");
+
+
+        //open Login Activity
+        Button buttonlogin = findViewById(R.id.button_login);
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //open register activity
+
+        Button buttonregister = findViewById(R.id.button_register);
+        buttonregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 
 }
